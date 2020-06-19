6631fac31068b9c0f193c1add820f62dc2b60cce
atif-majid <atif.majid10@gmail.com>
2020-06-19 07:29:17 +0500

    Completed the initial app development as per the video tutorial

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({transaction}) => {
+    const { deleteTransaction } = useContext(GlobalContext);
     const sign = transaction.amount  < 0 ? '-' : '+';
     const styleName = transaction.amount < 0 ? 'minus' : 'plus';
 
     return (<li className={styleName}>
     {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-    <button className="delete-btn">x</button>
+    <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
 </li>)
 }
